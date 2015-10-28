cb1b27edf00574f68d25fbe0c7e2dec6c907f1a2
etnbrd <etn@etnbrd.com>
2015-10-28 15:21:00 +0100

    fix build-config

--- build-config.sh
+++ build-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i.bak s/SHOUT_PASSWD/$SHOUT_PASSWD/g .shout/users/*.json
-sed -i.bak s/IRC_PASSWD/$IRC_PASSWD/g .shout/users/*.json
+sed -i s/SHOUT_PASSWD/$SHOUT_PASSWD/g .shout/users/*.json
+sed -i s/IRC_PASSWD/$IRC_PASSWD/g .shout/users/*.json
