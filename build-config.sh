2af601466ee6f222ee9727bb004d4b6e86297008
etnbrd <etn@etnbrd.com>
2015-10-28 15:29:22 +0100

    indentation on build_config

--- build-config.sh
+++ build-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sed -i s/SHOUT_PASSWD/$SHOUT_PASSWD/g .shout/users/*.json
-sed -i s/IRC_PASSWD/$IRC_PASSWD/g .shout/users/*.json
+sed -i s/IRC_PASSWD/$IRC_PASSWD/g     .shout/users/*.json
